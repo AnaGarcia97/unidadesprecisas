a28c6c2afb7dd4bdd25aff0e66a4af747561158f
AnaGarcia97 <124271484+AnaGarcia97@users.noreply.github.com>
2023-06-20 19:35:50 +0100

    Update script.js
    edição 2

--- script.js
+++ script.js
@@ -1,3 +1,48 @@
+/*cronómetro*/
+var cronometro;
+var segundos = 0;
+var minutos = 0;
+var horas = 0;
+
+function start() {
+  cronometro = setInterval(() => {
+    segundos++;
+    if (segundos == 60) {
+      segundos = 0;
+      minutos++;
+      if (minutos == 60) {
+        minutos = 0;
+        horas++;
+      }
+    }
+    document.getElementById("cronometro").innerHTML = formatTime(horas) + ":" + formatTime(minutos) + ":" + formatTime(segundos);
+    
+    // Verificar se passaram 5 minutos (300 segundos)
+    if (horas == 0 && minutos == 5 && segundos == 0) {
+      clearInterval(cronometro);
+      alert("O cronômetro atingiu 5 minutos.");
+    }
+  }, 1000);
+}
+
+function pause() {
+  clearInterval(cronometro);
+}
+
+function reset() {
+  clearInterval(cronometro);
+  segundos = 0;
+  minutos = 0;
+  horas = 0;
+  document.getElementById("cronometro").innerHTML = "00:00:00";
+}
+
+function formatTime(time) {
+  return time < 10 ? "0" + time : time; }
+
+
+
+  /*Roleta*/
 let container = document.querySelector(".container");
 let btn = document.getElementById("spin");
 let pointer = document.querySelector(".pointer");
@@ -10,5 +55,4 @@ btn.onclick = function () {
   container.style.transform = "rotate(" + rotationAngle + "deg)";
   pointer.style.transform = "rotate(" + rotationAngle + "deg)";
   score += number;
-  scoreElement.textContent = score;
-}
+  scoreElement.textContent = score;}
